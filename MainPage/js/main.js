ddb95628ea9d45bdc9a61c60872b9a625a9b5287
ClashKill <111309819+ClashKill@users.noreply.github.com>
2024-06-23 06:29:08 -0300

    Delete MainPage directory

--- MainPage/js/main.js
+++
@@ -1,9 +0,0 @@
-// main.js
-document.addEventListener('DOMContentLoaded', function () {
-    const navToggle = document.querySelector('.nav-toggle');
-    const navMenu = document.querySelector('.nav-menu');
-
-    navToggle.addEventListener('click', function () {
-        navMenu.classList.toggle('open');
-    });
-});
